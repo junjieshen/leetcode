581a587b994a79a4ee9a8f1504d38d20f36611a1
Junjie Shen <jshen6@ncsu.edu>
2015-12-23 14:58:15 -0500

    Refactor 014 Longest Common Prefix

--- src/014.LongestCommonPrefix/main.cc
+++ src/014.LongestCommonPrefix/main.cc
@@ -28,9 +28,30 @@ string longestCommonPrefix(vector<string>& strs) {
     return common_prefix;
 }
 
+string longestCommonPrefix_2(vector<string>& strs) {
+    if (strs.empty()) return "";
+
+    int shortest = strs[0].size();
+    for (int i = 1; i < strs.size(); i++) {
+        if (strs[i].size() < shortest) {
+            shortest = strs[i].size();
+        }
+    }
+
+    for (int i = 0; i < shortest; i++) {
+        char common_char = strs[0][i];
+        for (int j = 0; j < strs.size(); j++) {
+            if (strs[j][i] != common_char) {
+                return strs[j].substr(0, i);
+            }
+        }
+    }
+    return strs[0].substr(0, shortest);
+}
+
 int main() {
     vector<string> strs = {"whatisthis", "whatthefuck"};
-    string prefix = longestCommonPrefix(strs);
+    string prefix = longestCommonPrefix_2(strs);
     cout << prefix << endl;
 
     return 0;
