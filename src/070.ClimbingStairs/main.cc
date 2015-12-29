3da46ad7e20a5c88c41261f9352de7089471fb3a
Junjie Shen <jshen6@ncsu.edu>
2015-12-29 16:09:53 -0500

    add 070 Climb Stairs

---
+++ src/070.ClimbingStairs/main.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+long long climbStairs(int n) {
+    if (n <= 3) return n;
+
+    long long a = 1;
+    long long b = 2;
+    long long c = 3;
+
+    for (int i = 4; i <= n; i++) {
+        a = c;
+        c = a + b;
+        b = a;
+    }
+
+    return c;
+}
+
+int main() {
+    for (int i = 1; i <= 50; i++) {
+        long long n = climbStairs(i);
+        cout << i << " stairs, have " << n << " ways to climb\n";
+    }
+    return 0;
+}
